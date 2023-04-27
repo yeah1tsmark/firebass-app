36eddcc966aadcc2cec99e8dee498c8adbd78032
yeah1tsmark <marcwafula635@gmail.com>
2023-04-27 10:11:44 +0300

    added correct login and register code

--- app/src/main/java/com/example/firebase_mark/LoginActivity.kt
+++ app/src/main/java/com/example/firebase_mark/LoginActivity.kt
@@ -6,12 +6,15 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import android.widget.EditText
+import android.widget.Toast
+import com.google.firebase.auth.FirebaseAuth
 
 class LoginActivity : AppCompatActivity() {
     private lateinit var editemail:EditText
     private lateinit var editpass:EditText
     private lateinit var loginbutton:Button
     private lateinit var regbutton:Button
+    private lateinit var auth:FirebaseAuth
 
     @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -23,6 +26,8 @@ class LoginActivity : AppCompatActivity() {
         loginbutton = findViewById(R.id.btnlogin)
         regbutton = findViewById(R.id.btnreg)
 
+        auth = FirebaseAuth.getInstance()
+
         regbutton.setOnClickListener {
             // navigate user to registration page
             var gotoreg = Intent(this, RegisterActivity::class.java)
@@ -30,7 +35,29 @@ class LoginActivity : AppCompatActivity() {
         }
 
         loginbutton.setOnClickListener {
-            // verify
+            var email = editemail.text.toString().trim()
+            var pass = editpass.text.toString().trim()
+
+            // VALIDATION
+            if (email.isEmpty() || pass.isEmpty()){
+                Toast.makeText(this, "ERROR! Cannot submit an empty field", Toast.LENGTH_SHORT).show()
+
+            }else{
+                auth.signInWithEmailAndPassword(email,pass).addOnCompleteListener(this){
+                    if (it.isSuccessful){
+                        Toast.makeText(this, "Login Success!", Toast.LENGTH_SHORT).show()
+
+                        //navigate a use rt a diffdrent page
+                        var gotomain = Intent(this,MainActivity::class.java)
+                        startActivity(gotomain)
+                        // prevent user from going back to login page hence closing the app
+                        finish()
+
+                    }else{
+                        Toast.makeText(this, "Failed to login!", Toast.LENGTH_SHORT).show()
+                    }
+                }
+            }
         }
     }
 }
--- app/src/main/java/com/example/firebase_mark/RegisterActivity.kt
+++ app/src/main/java/com/example/firebase_mark/RegisterActivity.kt
@@ -1,16 +1,22 @@
 package com.example.firebase_mark
 
 import android.annotation.SuppressLint
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import android.widget.EditText
+import android.widget.Toast
+import com.google.firebase.auth.FirebaseAuth
 
 class RegisterActivity : AppCompatActivity() {
     private lateinit var editnamereg:EditText
     private lateinit var editregemail:EditText
     private lateinit var editregpass:EditText
     private lateinit var buttoncreateuser:Button
+
+    // initialize firebase
+    private lateinit var auth:FirebaseAuth
 
     @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -22,7 +28,35 @@ class RegisterActivity : AppCompatActivity() {
         editregpass = findViewById(R.id.edtregpass)
         buttoncreateuser = findViewById(R.id.btncreateuser)
 
+        //initialize again
+        auth = FirebaseAuth.getInstance()
+
         buttoncreateuser.setOnClickListener {
+
+            var name = editnamereg.text.toString().trim()
+            var email = editregemail.text.toString().trim()
+            var pass = editregpass.text.toString().trim()
+
+            // VALIDATION*
+            if (name.isEmpty() || email.isEmpty() || pass.isEmpty()){
+                Toast.makeText(this, "Error! Cannot submit an empty field", Toast.LENGTH_SHORT).show()
+
+            }else{
+                auth.createUserWithEmailAndPassword(email, pass).addOnCompleteListener(this){
+                    if (it.isSuccessful){
+                        Toast.makeText(this, "User created successfully", Toast.LENGTH_SHORT).show()
+
+                        // navigate back to login
+                        var gotologin = Intent(this, LoginActivity::class.java)
+                        startActivity(gotologin)
+
+                    }else{
+                        Toast.makeText(this, "Failed to create user", Toast.LENGTH_SHORT).show()
+                    }
+                }
+
+
+            }
         }
     }
 }
