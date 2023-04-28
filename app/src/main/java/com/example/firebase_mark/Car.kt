30162608bab94cbb832fc06d6ce7cb0f86afaa4d
yeah1tsmark <marcwafula635@gmail.com>
2023-04-28 10:47:13 +0300

    modified it to show car data

---
+++ app/src/main/java/com/example/firebase_mark/Car.kt
@@ -0,0 +1,16 @@
+package com.example.firebase_mark
+
+class Car {
+    var carmake:String?= null
+    var carmodel:String?= null
+    var carprice:String?= null
+
+    constructor(carmake:String, carmodel:String, carprice:String){
+        this.carmake = carmake
+        this.carmodel = carmodel
+        this.carprice = carprice
+    }
+
+    constructor()
+
+}
--- app/src/main/java/com/example/firebase_mark/MainActivity.kt
+++ app/src/main/java/com/example/firebase_mark/MainActivity.kt
@@ -10,27 +10,79 @@ import com.google.firebase.database.FirebaseDatabase
 
 class MainActivity : AppCompatActivity() {
 
-    private lateinit var editdata:EditText
-    private lateinit var savebutton:Button
+    private lateinit var editcarmodel:EditText
+    private lateinit var editcarmake:EditText
+    private lateinit var editcarprice:EditText
+    private lateinit var butttonuploadimg:Button
+    private lateinit var buttonuploaddata:Button
+    private lateinit var buttonview:Button
 
     @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        editdata = findViewById(R.id.edtdata)
-        savebutton = findViewById(R.id.btnsave)
+        editcarmodel = findViewById(R.id.edtcarmodel)
+        editcarmake = findViewById(R.id.edtcarmake)
+        editcarprice = findViewById(R.id.edtcarprice)
+        butttonuploadimg = findViewById(R.id.btnuploadimg)
+        buttonuploaddata = findViewById(R.id.btnuploaddata)
+        buttonview = findViewById(R.id.btnviewcars)
 
+        // initialize
         var database = FirebaseDatabase.getInstance()
-        var databaseRef = database.reference
+        var databaseRef = database.getReference("cars")
 
-        savebutton.setOnClickListener {
-            var user_data = editdata.text.toString().trim()
-            //Toast.makeText(this, "user_data", Toast.LENGTH_SHORT).show()
-            databaseRef.setValue(user_data)
+        // uploading all data
+        buttonuploaddata.setOnClickListener {
+
+            var carmake = editcarmake.text.toString().trim()
+            var carmodel = editcarmodel.text.toString().trim()
+            var carprice = editcarprice.text.toString().trim()
+
+            // validation
+            if (carmake.isEmpty() || carmodel.isEmpty() || carprice.isEmpty()) {
+                Toast.makeText(this, "Error! Cannot upload empty fields", Toast.LENGTH_SHORT).show()
+            } else {
+                // try to upload data
+                var usercar = Car(carmake, carmodel, carprice)
+
+                // creating a reference to FirebaseDatabase
+                var ref = FirebaseDatabase.getInstance().getReference().child("cars")
+
+                ref.setValue(usercar).addOnCompleteListener {
+
+                    if (it.isSuccessful) {
+                        Toast.makeText(
+                            this, "Car Data Uploaded Successfully",
+                            Toast.LENGTH_LONG
+                        ).show()
+                    } else {
+                        Toast.makeText(
+                            this, "Failed tO Save Car Info",
+                            Toast.LENGTH_LONG
+                        ).show()
+                    }
+
+                }
+
+            }
+
+            // uploading img
+            butttonuploadimg.setOnClickListener {
+
+
+            }
+
+
+            // viewing uploaded img
+            buttonview.setOnClickListener {
+                //var user_data = editdata.text.toString().trim()
+                //Toast.makeText(this, "user_data", Toast.LENGTH_SHORT).show()
+                //databaseRef.setValue(user_data)
+            }
+
 
         }
-
-
     }
 }
